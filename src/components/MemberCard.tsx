e3caddd2972e4cda3c4ae8bfb56be30ba92991be
Tristan Simpson <heytristaann@gmail.com>
2024-02-26 18:48:16 -0500

    update

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function MemberCard(props: { user: User }): JSX.Element {
   return (
-    <div className="flex h-72 w-60 flex-col items-center justify-center gap-2 rounded-lg border border-primary bg-secondary duration-300 ease-in-out hover:scale-105 hover:shadow-md hover:shadow-emerald-700/50">
+    <div className="flex h-80 w-full flex-col items-center justify-center gap-2 rounded-lg border border-primary bg-secondary p-2 duration-300 ease-in-out hover:scale-105 hover:shadow-md hover:shadow-emerald-700/50 sm:w-60">
       <Image
         src={props.user.image}
-        alt="Profile picture"
+        alt="..."
         className="h-28 w-28 rounded-full"
         width={500}
         height={500}
@@ -16,7 +16,7 @@ export default function MemberCard(props: { user: User }): JSX.Element {
         <h1 className="text-xl font-semibold text-white">{props.user.name}</h1>
         <p className="text-sm font-thin text-white">{props.user.email}</p>
 
-        <div className="mt-3 flex flex-wrap items-center justify-center gap-3">
+        <div className="mt-3 flex flex-wrap items-center justify-center gap-1.5">
           {props.user.roles.slice(1).map((role) => (
             <div className="w-fit rounded-md border border-primary bg-emerald-950/50 px-2 py-1 text-xs font-thin text-white">
               {role}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 // import { nextui } from "@nextui-org/react";
 
 export default {
-  content: [
-    "./src/**/*.tsx",
-    "./src/app/**/*.tsx",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.tsx", "./src/app/**/*.tsx"],
   theme: {
     extend: {
       colors: {
